e854e404e8836291f84e7c41661653289f18de81
silasbillum <stilhofsvej46@gmail.com>
2025-08-12 11:06:20 +0200

    Make logo clickable and navigate to home in navbar
    Replaced the static 'Mercantec Space' text in the navbar with a Button that navigates to the home page when clicked, improving user navigation. Minor formatting

--- React/frontend/src/components/templates/navbar.tsx
+++ React/frontend/src/components/templates/navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Button } from "@/components/ui/button"
 import { useAuth } from "@/contexts/AuthContext"
 import { useState } from "react"
@@ -8,7 +8,7 @@ import { ModeToggle } from "@/components/ui/mode-toggle"
 export default function Navbar() {
     const { isAuthenticated } = useAuth()
     const [sidebarOpen, setSidebarOpen] = useState(false)
-
+    const navigate = useNavigate()
     return (
         <nav className="w-full py-4 bg-white text-black flex items-center px-6">
             {/* Left: Burger menu */}
@@ -80,13 +80,13 @@ export default function Navbar() {
             <div className="flex flex-1 justify-center">
                 <div className="flex items-center pl-86">
                     <img src="/src/components/images/mercantec-space-logo.png" alt="Mercantec Space Logo" className="h-8 w-8 object-contain" />
-                    <span className="ml-1 font-semibold text-lg">Mercantec Space</span>
+                    <Button onClick={() => navigate("/")} className="ml-1 font-semibold text-lg hover:text-gray-600 active:scale-95 transition-transform cursor-pointer bg-transparent border-0 shadow-none text-blue">Mercantec Space</Button>
                 </div>
             </div>
 
             {/* Right: Teams and Discord buttons */}
             <div className="flex gap-1 justify-end min-w-[180px] mr-2">
-                <a  
+                <a
                     href="https://teams.microsoft.com/l/team/19%3AILE0F1oc9fGJMPT6mFVjcQ7N10W1SE8XA9r1X4N6Pko1%40thread.tacv2/conversations?groupId=a014d18d-572a-4621-bf7d-682f06ec1fea&tenantId=17aab4ce-4b26-487e-9bea-1e2a70348bf0"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -121,7 +121,7 @@ export default function Navbar() {
                     )}
                 </Button>
                 <Button asChild className="bg-black text-white hover:bg-gray-600 w-24">
-                {isAuthenticated ? (
+                    {isAuthenticated ? (
                         <Link to="/logout">Logout</Link>
                     ) : (
                         <Link to="/signup">Signup</Link>
